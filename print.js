090e5156df006791c9610e8e5337ccacb09499d3
uedymd <ueda@kous.co.jp>
2020-06-16 23:19:50 +0900

    テスト第一弾

---
+++ print.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // PDF出力対象ページ
+  let loadPromise = page.waitForNavigation();
+  await page.goto('https://site-analytics.jp/report_convert/index_json.php?test=1');
+  await loadPromise;
+
+  // PDF作成処理
+  await page.pdf({
+    path: './pdf/groweb.pdf',
+    displayHeaderFooter: true,
+    printBackground: true,
+    format: 'A4',
+    landscape: true,
+  });
+
+  browser.close();
+  console.log('PDF出力完了');
+})();
