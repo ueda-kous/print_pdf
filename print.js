f4bf35ba516c7699915f18174d1e59c97805083f
uedymd <ueda@kous.co.jp>
2020-06-17 10:27:44 +0900

    システム連動バグ修正

--- print.js
+++ print.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 const targetUrl = process.argv[2];
 const fileName  = process.argv[3];
 
+
 (async() => {
   const browser = await puppeteer.launch({
     args: [
@@ -15,13 +16,12 @@ const fileName  = process.argv[3];
 
   // PDF出力対象ページ
   let loadPromise = page.waitForNavigation();
-//   await page.goto('https://site-analytics.jp/report_convert/index_json.php?test=1');
   await page.goto(targetUrl);
   await loadPromise;
 
   // PDF作成処理
   await page.pdf({
-    path: './'+fileName+'.pdf',
+    path: './pdf/'+fileName+'.pdf',
     displayHeaderFooter: true,
     printBackground: true,
     format: 'A4',
